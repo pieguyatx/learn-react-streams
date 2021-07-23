b73dfcd38d7cb409c3bc121f5c0c04042c07e0fe
Pius Wong <pius@pioslabs.com>
2021-07-23 17:33:07 -0500

    Set up google oauth component initial parts

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -1,6 +1,15 @@
 import React from 'react';
 
 class GoogleAuth extends React.Component {
+    componentDidMount() {
+       window.gapi.load('client:auth2', () => {
+        window.gapi.client.init({
+            clientId: '26481134499-t930p841d8s9gcm18ctt1q4ljqv42nkm.apps.googleusercontent.com',
+            scope: 'email'
+        });
+       }); // callback fxn only executes after additional JS code downloaded from Google server
+    }
+
     render() {
         return <div>Google Auth</div>
     }
