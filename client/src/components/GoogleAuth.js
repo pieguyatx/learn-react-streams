f30ba8a03fd5f271d7f049a44731e01eb08eddf2
Pius Wong <pius@pioslabs.com>
2021-07-23 18:00:13 -0500

    Notify when signed in with OAuth

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -1,17 +1,32 @@
 import React from 'react';
 
 class GoogleAuth extends React.Component {
+    state = { isSignedIn: null };
+
     componentDidMount() {
        window.gapi.load('client:auth2', () => {
         window.gapi.client.init({
             clientId: '26481134499-t930p841d8s9gcm18ctt1q4ljqv42nkm.apps.googleusercontent.com',
             scope: 'email'
+        }).then(() => {
+            this.auth = window.gapi.auth2.getAuthInstance(); // define the Auth object when ready
+            this.setState({ isSignedIn: this.auth.isSignedIn.get() });
         });
        }); // callback fxn only executes after additional JS code downloaded from Google server
     }
 
+    renderAuthButton() {
+        if (this.state.isSignedIn === null) {
+            return <div>Dunno if anyone's signed in!</div>;
+        } else if (this.state.isSignedIn) {
+            return <div>Signed in!</div>
+        } else {
+            return <div>Not signed in!</div>
+        }
+    }
+
     render() {
-        return <div>Google Auth</div>
+        return <div>{ this.renderAuthButton() }</div> // need the () on the function here
     }
 }
 
