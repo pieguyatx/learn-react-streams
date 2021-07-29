bd664a3756a1daa657b508a2e4577dac3ba9ffc3
Pius Wong <pius@pioslabs.com>
2021-07-29 14:07:32 -0500

    Create reducers for auth

---
+++ client/src/reducers/authReducer.js
@@ -0,0 +1,14 @@
+const INITIAL_STATE = {  // all caps syntax is a convention to say its supposed to be constant
+    isSignedIn: null
+};
+
+export default (state = INITIAL_STATE, action) => {
+    switch(action.type){
+        case 'SIGN_IN':
+            return {...state, isSignedIn: true}
+        case 'SIGN_OUT':
+            return {...state, isSignedIn: false}
+        default:
+            return state;
+    }
+};
