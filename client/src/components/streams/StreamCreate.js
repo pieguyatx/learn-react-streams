433ec31c98d69182558141ed89e241af28f8d45f
Pius Wong <pius@pioslabs.com>
2021-08-03 19:10:05 -0500

    Forms (from redux-form) skeleton added to 'new stream' page

--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import { Field, reduxForm } from 'redux-form'; // Field is a react component; reduxForm is a function
 
-const StreamCreate = () => {
-    return (
-        <div>StreamCreate</div>
-    );
-};
+class StreamCreate extends React.Component {
+    // every react input element should be "controlled"...
+    renderInput( {input} ) {
+        // shortened syntax for redux-form adds all properties of input as props of that component:
+        return (
+            <input {...input} />
+        );
+    }
 
-export default StreamCreate;
+    render() {
+        return (
+            <form>
+                <Field name="title" component={this.renderInput} />
+                <Field name="description" component={this.renderInput} />
+            </form>
+        );
+    }
+}
+
+export default reduxForm({
+    form: 'streamCreate'
+})(StreamCreate);
