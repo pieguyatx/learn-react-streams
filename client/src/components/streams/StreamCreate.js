fe82d37037ff4a8dd1669619e50c15e7a8aebf85
Pius Wong <pius@pioslabs.com>
2021-08-30 18:06:01 -0500

    Form shows validation messages

--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -3,12 +3,13 @@ import { Field, reduxForm } from 'redux-form'; // Field is a react component; re
 
 class StreamCreate extends React.Component {
     // every react input element should be "controlled"...
-    renderInput( {input, label} ) {
+    renderInput( {input, label, meta} ) {
         // shortened syntax for redux-form adds all properties of input as props of that component:
         return (
             <div className="field">
                 <label>{label}</label>
                 <input {...input} />
+                <div>{meta.error}</div>
             </div>
         );
     }
@@ -29,6 +30,20 @@ class StreamCreate extends React.Component {
     }
 }
 
+// Validation function for Redux Forms
+const validate = (formValues) => {
+    const errors = {};
+    if (!formValues.title) {
+        // only when no title entered...
+        errors.title = 'You must enter a title.';
+    }
+    if (!formValues.description) {
+        errors.description = 'You must enter a description.';
+    }
+    return errors;
+};
+
 export default reduxForm({
-    form: 'streamCreate'
+    form: 'streamCreate',
+    validate // equivalent to:    validate: validate
 })(StreamCreate);
