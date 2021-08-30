bc1f9dd8875cd24c50e91ca2ab23e36422950bb7
Pius Wong <pius@pioslabs.com>
2021-08-30 17:48:10 -0500

    Form in 'new streams' page has labels

--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -3,18 +3,21 @@ import { Field, reduxForm } from 'redux-form'; // Field is a react component; re
 
 class StreamCreate extends React.Component {
     // every react input element should be "controlled"...
-    renderInput( {input} ) {
+    renderInput( {input, label} ) {
         // shortened syntax for redux-form adds all properties of input as props of that component:
         return (
-            <input {...input} />
+            <div className="field">
+                <label>{label}</label>
+                <input {...input} />
+            </div>
         );
     }
 
     render() {
         return (
-            <form>
-                <Field name="title" component={this.renderInput} />
-                <Field name="description" component={this.renderInput} />
+            <form className="ui form">
+                <Field name="title" component={this.renderInput} label="Enter Title" />
+                <Field name="description" component={this.renderInput} label="Enter Description" />
             </form>
         );
     }
