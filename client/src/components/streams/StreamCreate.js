5f38c8f5193132b998a1a1cb87b081502b3059b9
Pius Wong <pius@pioslabs.com>
2021-08-30 17:53:28 -0500

    Redux Form sends form values to functions

--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -13,11 +13,17 @@ class StreamCreate extends React.Component {
         );
     }
 
+    onSubmit(formValues) {
+        // event.preventDefault();  // <-- this is handled by Redux Form
+        console.log(formValues);
+    }
+
     render() {
         return (
-            <form className="ui form">
+            <form className="ui form" onSubmit={this.props.handleSubmit(this.onSubmit)}>
                 <Field name="title" component={this.renderInput} label="Enter Title" />
                 <Field name="description" component={this.renderInput} label="Enter Description" />
+                <button className="ui button primary">Submit</button>
             </form>
         );
     }
